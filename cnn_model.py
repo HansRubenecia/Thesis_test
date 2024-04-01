9223f3fecd4a055198fb6293a86ab518d9b8f493
HansRubenecia <hans.rubenecia@neu.edu.ph>
2024-04-01 11:46:38 +0800

    first commit

---
+++ cnn_model.py
@@ -0,0 +1,20 @@
+# Define the model architecture
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.fc1 = nn.Linear(64 * 54 * 54, 64)
+        self.fc2 = nn.Linear(64, num_classes)  # Output layer size equals the number of classes
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
